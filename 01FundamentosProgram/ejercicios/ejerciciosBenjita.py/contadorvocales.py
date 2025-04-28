bed4987f5c54e6b5d165d94cabdf65e6005334d3
MichiLunaa <189772667+MichiLunaa@users.noreply.github.com>
2025-04-28 11:44:33 -0400

    commit:)

--- 01FundamentosProgram/ejercicios/ejerciciosBenjita.py/contadorvocales.py
+++ 01FundamentosProgram/ejercicios/ejerciciosBenjita.py/contadorvocales.py
@@ -1,2 +1,7 @@
 #Pide al usuario una palabra y muestra cuántas vocales contiene.
-i
+palabra = input("Introduce una palabra: ")
+vocales = 0
+for letra in palabra:
+    if letra in "aeiouAEIOU":
+        vocales += 1
+print(f"La palabra contiene {vocales} vocales.")
