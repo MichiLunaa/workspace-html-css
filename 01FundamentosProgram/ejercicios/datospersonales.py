69627118ad8abecd65f9beecace67da4bd962d6b
MichiLunaa <189772667+MichiLunaa@users.noreply.github.com>
2025-04-28 12:23:35 -0400

    commit:)

---
+++ 01FundamentosProgram/ejercicios/datospersonales.py
@@ -0,0 +1,9 @@
+#consultar datos personales
+Nombre = input("ingrese su nombre: ")
+Rut = input("ingrese su rut: ")
+Correo = input("ingrese su correo: ")
+Telefono = int(input("ingrese su telefono: "))
+print("NOMBRE: ", Nombre)
+print("RUT: ", Rut)
+print("CORREO: ", Correo)
+print("TELEFONO: ", Telefono)
---
+++ 01FundamentosProgram/ejercicios/funcion.py
@@ -0,0 +1,4 @@
+#Utilizando un ingreso de variables de tipo numérica con Int (input()), se pide el valor de X para resolver la siguiente ecuación: x^2 + 3x + 1 :/ 4 = 0
+Valorx= int(input("Ingrese el valor de x: "))
+resultado = ((Valorx**2) + 3*Valorx + 1)/ 4 
+print("El resultado de la ecuación es:", resultado)
